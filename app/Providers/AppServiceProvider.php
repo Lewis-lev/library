abc350c7b7f446a2d7f3cbe65e8b9c16113cd98c
lewis <madanishofa@gmail.com>
2025-05-19 15:12:53 +0700

    Another testing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,11 @@
+
 <?php
 
 namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,15 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
-        if (env('APP_ENV') == 'production') {
-            $url->forceScheme('https');
-            SymfonyRequest::setTrustedProxies(['*'],
-                SymfonyRequest::HEADER_X_FORWARDED_FOR
-                | SymfonyRequest::HEADER_X_FORWARDED_HOST
-                | SymfonyRequest::HEADER_X_FORWARDED_PROTO
-                | SymfonyRequest::HEADER_X_FORWARDED_PORT
-                | SymfonyRequest::HEADER_X_FORWARDED_PREFIX
-            );
-        }
+         if (app()->environment('production')) {
+         URL::forceScheme('https');
+    }
     }
 }
