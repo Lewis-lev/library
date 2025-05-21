9297ec82a3ef83c35ff477ef16dce5500aeb1627
lewis <madanishofa@gmail.com>
2025-05-21 09:08:58 +0700

    Another testing

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = '*'; // Use '*' to trust all proxies
+    protected $proxies = ['192.168.1.1', '10.0.0.0/8']; // Use '*' to trust all proxies
 
     /**
      * The headers that should be used to detect proxies.
